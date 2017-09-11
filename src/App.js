b0ee89b4439f2bc15d54ad4a39eac3f650bc2b12
Zola <zolamcadie@gmail.com>
2017-09-11 03:49:51 +0000

    Add react

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>Hello from react</div>
+    );
+  }
+}
+export default App;
--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+document.addEventListener('DOMContentLoaded', function() {
+  ReactDOM.render(
+    React.createElement(App),
+    document.getElementById('react-root')
+  );
+});
